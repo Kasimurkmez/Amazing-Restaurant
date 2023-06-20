d996648131e59452ba475b1f552e3c928076f099
kasimurkmez <kasimurkmez47@gmail.com>
2023-06-20 13:39:00 +0300

    Reactjs restaurant için gerekli kurulumlar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-lime-500">Merhaba</h1>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+    </>
+  );
+}
+export default App;
